64057dac372174118f80d52eec3ce5184a1f0eb0
BlushBee <73192531+BlushBee@users.noreply.github.com>
2020-11-02 21:31:42 +0100

    Add files via upload

---
+++ wwwroot/js/connection.js
@@ -0,0 +1,146 @@
+﻿
+
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/drawhub")
+    .withHubProtocol(new signalR.protocols.msgpack.MessagePackHubProtocol())
+    .configureLogging(signalR.LogLevel.Information)
+    .withAutomaticReconnect([0, 0, 10000])
+    .build();
+
+var connId;
+async function start() {
+    try {
+        await connection.start();
+        console.assert(connection.state === signalR.HubConnectionState.Connected);
+
+        try {
+
+            await connection.invoke('getConnectionId')
+                .then(function (id) {
+                    connId = id;
+                });
+
+            // console.log(connId);
+            // await connection.invoke("PingPong");
+
+            const params = new URLSearchParams(window.location.search);
+
+            const invite = params.get('invite');
+            if (invite != undefined) {
+                inviteLink = invite;
+                await connection.invoke("JoinRoomWithInviteUrl", connId, inviteLink);
+            }
+            else {
+                const room = params.get('room');
+                if (room != undefined) {
+                    document.getElementById("roomInputName").value = room;
+                }
+                joinRoom();
+            }
+        } catch (err) {
+            console.error(err);
+        }
+        console.log("Connected.");
+    } catch (err) {
+        console.assert(connection.state === signalR.HubConnectionState.Disconnected);
+        console.log(err);
+        setTimeout(() => start(), 5000);
+    }
+};
+
+connection.onreconnecting(async error => {
+    console.assert(connection.state === signalR.HubConnectionState.Reconnecting);
+    console.log(`Connection lost due to error "${error}". Reconnecting.`);
+});
+
+connection.onclose(async error => {
+    console.assert(connection.state === window.signalR.HubConnectionState.Disconnected);
+    console.log(`Connection closed due to error "${error}". Try refreshing this page to restart the connection.`);
+});
+
+connection.on("pingpong", async (message) => {
+    console.log(`"pingpong: ${convertUint8ArrayToJson(message)}"`);
+});
+
+connection.on("undoDrawAction", async (stroke) => {
+    var obj = convertUint8ArrayToObject(stroke);
+    await undoDrawAction(obj[0], obj[1]);
+});
+
+connection.on("setClearPlayerName", async () => {
+    contextOverlay.clearRect(0, 0, canvasOverlay.width, canvasOverlay.height);
+});
+
+connection.on("recieveChatRoomMessage", async (chatMessage) => {
+    var obj = convertUint8ArrayToObject(chatMessage);
+    createChatRoomMessage(obj.from, obj.message);
+});
+
+connection.on("setMessage", async (message) => {
+    var obj = convertUint8ArrayToObject(message);
+    document.getElementById('roomMessage').innerText = obj.message;
+
+    // to do add message types
+    if (obj.message === "Wrong password") {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        document.getElementById('roomName').innerText = "-";
+        document.getElementById('playerCount').innerText = "-";
+        document.getElementById('playerName').innerText = "not connected";
+        window.isInRoom = false;
+    }
+});
+
+connection.on("resetRoom", () => {
+    ResetCanvas(context);
+});
+
+connection.on("updatePlayerCount", async (count) => {
+    var obj = convertUint8ArrayToObject(count);
+    document.getElementById('playerCount').innerText = obj;
+});
+
+//todo update server side to send it as one object
+connection.on("setupRoom", async (roomName, description, playerCount, inviteLink, history) => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    window.localDrawingHistory = [];
+    window.remoteDrawingHistory = [];
+    window.isInRoom = true;
+
+
+    document.getElementById("roomName").innerText = roomName;
+    document.getElementById("roomInputName").value = roomName;
+    document.getElementById("playerCount").innerText = playerCount;
+    document.getElementById("inviteUrl").innerText = " | Room invite link: " + window.location.protocol + "//" + document.location.host + document.location.pathname + "?invite=" + inviteLink;
+    document.getElementById("playerName").innerText = connId;
+    document.getElementById("chatRoomInfo").innerHTML = 'Welcome to #' + roomName + '<br>Info: ' + description;
+    var obj = convertUint8ArrayToObject(history);
+    await obj.forEach(async function (item) {
+        window.remoteDrawingHistory.push(item);
+        var hexColor =  rgbToHex(item.color[0],item.color[1],item.color[2]);
+        await setStroke(item.stroke[0] + canvasOffsetX, item.stroke[1] + canvasOffsetY, item.stroke[2]+ canvasOffsetX, item.stroke[3] + canvasOffsetY, item.size, hexColor);
+    });
+});
+
+// update from remote players
+connection.on("updateRoomPlayersDrawAction", async (drawAction) => {
+    var obj = convertUint8ArrayToObject(drawAction);
+    remoteDrawingHistory.push(obj);
+
+    if (document.querySelector('#showPlayerName').checked) {
+        var x = obj.stroke[2] + canvasOffsetX - (context.measureText(obj.connId).width / 2);
+        var y = obj.stroke[3] + canvasOffsetY - 20;
+        await drawPlayerName(canvasOverlay, contextOverlay, x, y, obj.connId);
+    }
+   var hexColor =  rgbToHex(obj.color[0],obj.color[1],obj.color[2]);
+    await setStroke(obj.stroke[0]+ canvasOffsetX, obj.stroke[1] + canvasOffsetY, obj.stroke[2] + canvasOffsetX, obj.stroke[3] + canvasOffsetY, obj.size, hexColor);
+});
+
+
+// Start the connection.
+start();
+
+
+
+
+
---
+++ wwwroot/js/drawtogether.js
@@ -0,0 +1,443 @@
+﻿var localDrawingHistory = [];
+var roomPlayers = []; // keep track of players in room in preparation of adding ban / kick / mute or remove player drawing from room
+var remoteDrawingHistory = []; // offload history to client + add identifier username -> alias (to hide logged in user)
+var enableShowRemoteDrawingHistory = true; // todo when enabled hide remote people drawing but keep history
+var isInRoom = false;
+var strokeStartFrom = [0, 0];
+var strokeEndAt = [0, 0];
+var undoCollectionId = 0;
+var setUndoCollectionId = true;
+var selectedSize = 2;
+var selectedColor = "#000";
+var footer;
+var canvas;
+var canvasOverlay;
+var context;
+var contextOverlay;
+var canvasOffsetX = 0; // these are for scrolling canvas offsets
+var canvasOffsetY = 0;
+var inviteLink;
+var container = document.getElementById("container");
+var canvasWrapper = document.getElementById("canvasWrapper");
+var chatRoomOffsetY = 150;
+var footerOffsetY = 120;
+
+$(window).on('load',
+    function () {
+        canvas = createCanvas("canvas");
+        context = canvas.getContext("2d");
+
+        canvasOverlay = createCanvas("canvasOverlay");
+        contextOverlay = canvasOverlay.getContext("2d");
+        //canvasOverlay.style.cursor = "url('images/cursor.png') 18 18, auto";
+
+        canvasOverlay.addEventListener("mousedown", function (e) {
+            startDraw(e);
+
+        });
+        canvasOverlay.addEventListener("mousemove", function (e) {
+            startWhileDraw(e);
+        });
+
+        canvasOverlay.addEventListener("mouseup", async function () {
+            drawEnd();
+        });
+
+        //todo add support for touch + tablet
+        //canvasOverlay.addEventListener("pointerdown", async function (e) {
+        //    startDraw(e);
+        //});
+        //canvasOverlay.addEventListener("pointermove", async function (e) {
+        //    startWhileDraw(e);
+        //});
+        //canvasOverlay.addEventListener("pointerup", async function (e) {
+        //    drawEnd();
+        //});
+
+        //canvasOverlay.addEventListener("touchstart", async function (e) {
+        //    startDraw(e);
+        //});
+        //canvasOverlay.addEventListener("touchmove", async function (e) {
+        //    startWhileDraw(e);
+        //});
+        //canvasOverlay.addEventListener("touchend", async function (e) {
+        //    drawEnd();
+        //});
+
+        canvasWrapper.appendChild(canvas);
+        canvasWrapper.appendChild(canvasOverlay);
+
+        resizeCanvas(canvas);
+        resizeCanvas(canvasOverlay);
+
+        footer = createFooter(120);
+        container.appendChild(footer);
+
+        resizeChatRoom(chatRoomOffsetY);
+        resizeFooter(footerOffsetY);
+
+        redraw(localDrawingHistory);
+        redraw(remoteDrawingHistory);
+    });
+
+window.addEventListener("resize", function () {
+    resizeCanvas(canvas);
+    resizeCanvas(canvasOverlay);
+    resizeChatRoom(chatRoomOffsetY);
+    resizeFooter(footerOffsetY);
+    redraw(localDrawingHistory);
+    redraw(remoteDrawingHistory);
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.ctrlKey && event.key === 'z') {
+        undo();
+    }
+});
+
+$("#roomInputName").on('keyup', function (e) {
+    if (e.key === 'Enter' || e.keyCode === 13) {
+        joinRoom();
+    }
+});
+
+$("#chatRoomInputMessage").focus(function () {
+    $(this).val("");
+});
+
+$("#chatRoomInputMessage").focusout(function () {
+    $(this).val("Type here to send a message to the room");
+});
+
+$("#chatRoomInputMessage").on('keyup', function (e) {
+    if ($(this) != undefined && $(this) != null && $(this) !== "") {
+        if (e.key === 'Enter' || e.keyCode === 13) {
+            var message = document.getElementById('chatRoomInputMessage').value;
+            if (message.trim().length) {
+                sendChatRoomMessage();
+                $(this).val("");
+            }
+        }
+    }
+});
+
+
+$("#roomInputPassword").on('keyup', function (e) {
+    if (e.key === 'Enter' || e.keyCode === 13) {
+        joinRoom();
+    }
+});
+
+
+
+
+function createCanvas(id) {
+
+    console.log(`createCanvas(${id})`);
+
+    var canvas = document.createElement('canvas');
+    canvas.style.border = "2px solid #000";
+    canvas.setAttribute("id", id);
+    console.log(canvas);
+    return canvas;
+}
+
+
+function startDraw(e) {
+    if (isInRoom) {
+        if (setUndoCollectionId) {
+            undoCollectionId += 1;
+        }
+        setUndoCollectionId = false;
+        if (e.buttons === 1) {
+            getMousePosition(canvasOverlay, e);
+            canvasOffsetX = canvasOffsetX + (strokeStartFrom[0] - strokeEndAt[0]);
+            canvasOffsetY = canvasOffsetY + (strokeStartFrom[1] - strokeEndAt[1]);
+            updatePlayerDrawAction();
+            strokeEndAt = strokeStartFrom;
+        }
+    }
+}
+
+
+function startWhileDraw(e) {
+    if (isInRoom) {
+        strokeStartFrom = getMousePosition(canvasOverlay, e);
+        if (e.buttons === 1) {
+            updatePlayerDrawAction();
+        }
+        else if (e.buttons === 4) { // middle button
+            canvasOffsetX = canvasOffsetX + (strokeStartFrom[0] - strokeEndAt[0]);
+            canvasOffsetY = canvasOffsetY + (strokeStartFrom[1] - strokeEndAt[1]);
+
+            context.clearRect(0, 0, canvasOverlay.width, canvasOverlay.height);
+            contextOverlay.clearRect(0, 0, canvasOverlay.width, canvasOverlay.height);
+            redraw(remoteDrawingHistory);
+            redraw(localDrawingHistory);
+        }
+        strokeEndAt = strokeStartFrom;
+    }
+}
+
+
+async function drawEnd() {
+    if (isInRoom) {
+        setUndoCollectionId = true;
+        await connection.invoke("setClearPlayerName", connId);
+    }
+}
+
+
+
+async function updatePlayerDrawAction() {
+    var rgbColor = hexToRgb(selectedColor);
+    var action = [
+        strokeEndAt[0] - canvasOffsetX,
+        strokeEndAt[1] - canvasOffsetY, 
+        strokeStartFrom[0]- canvasOffsetX,
+        strokeStartFrom[1] - canvasOffsetY
+    ];
+    var stroke = { connId: connId, id: undoCollectionId, size: selectedSize, stroke: action, color: rgbColor};
+
+    //pass the local offset to stroke
+    setStroke(stroke.stroke[0] + canvasOffsetX, stroke.stroke[1] + canvasOffsetY, stroke.stroke[2] + canvasOffsetX, stroke.stroke[3] + canvasOffsetY, selectedSize, selectedColor);
+    localDrawingHistory.push(stroke);  
+
+    //var bytes = msgpack5({ forceFloat64: true }).encode(stroke);
+    //console.log(bytes);
+    if (connection.state === signalR.HubConnectionState.Connected) {
+        await connection.invoke("SetDrawAction", connId, JSON.stringify(stroke));
+    }
+}
+
+function setStroke(x1, y1, x2, y2, size, color) {
+    context.lineCap = "round";
+    context.lineWidth = size;
+    context.strokeStyle = color;
+
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+}
+
+
+function drawPlayerName(canvas, context, x, y, name) {
+    console.log("drawPlayerName()");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.font = "14px Arial";
+    context.fillText(name, x, y);
+}
+
+
+async function undo() {
+    undoCollectionId = 0;
+
+    localDrawingHistory.forEach(function (item) {
+        if (item.id > undoCollectionId) {
+            undoCollectionId = item.id;
+        }
+    });
+
+    localDrawingHistory = localDrawingHistory.filter(item => !(item.id === undoCollectionId));
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    redraw(remoteDrawingHistory);
+    redraw(localDrawingHistory);
+
+    if (connection.state === signalR.HubConnectionState.Connected) {
+        await connection.invoke("UndoDrawAction", connId, undoCollectionId);
+    }
+}
+
+
+
+var oldRoomName;
+var oldRoomPassword;
+async function joinRoom() {
+
+    var roomInputName = document.getElementById("roomInputName").value;
+    var roomInputPassword = document.getElementById("roomInputPassword").value;
+
+    if ((roomInputName !== oldRoomName || roomInputPassword !== oldRoomPassword)) {
+        oldRoomName = roomInputName;
+        oldRoomPassword = roomInputPassword;
+        document.getElementById('roomName').innerText = roomInputName;
+        window.history.pushState({ page: "another" }, "another page", window.location);
+
+        canvasOffsetX = 0;
+        canvasOffsetY = 0;
+
+        var url = window.location.protocol + "//" + document.location.host + document.location.pathname + "?room=" + document.getElementById("roomInputName").value;
+        window.history.pushState('page2', 'Title', url);
+
+        //todo we could do this in one call -> return entire room info instead of just player count in room?
+        await connection.invoke("UpdateClientRoomOnConnect", connId, roomInputName, roomInputPassword);
+    }
+}
+
+
+function redraw(drawHistory) {
+    console.log("redraw()");
+    drawHistory.forEach(function (item) {
+        var hexColor =  rgbToHex(item.color[0],item.color[1],item.color[2]);
+        setStroke(item.stroke[0] + canvasOffsetX, item.stroke[1] + canvasOffsetY, item.stroke[2] + canvasOffsetX, item.stroke[3] + canvasOffsetY, item.size, hexColor);
+    });
+}
+
+function resizeCanvas(canvas) {
+    console.log("resizeCanvas()");
+    //context.canvas.width = 1280;
+    //context.canvas.height = 720;
+
+    canvas.width = innerWidth - 440;
+    canvas.height = innerHeight - 150;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+
+function ResetCanvas(context) {
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+//note when resizing an element that has been created previously you can notice the resizing on load of page *that's why i opted ro create the elements instead and then resize)
+// needs to be done properly when have time
+function resizeChatRoom(offsetY) {
+    console.log("resizeChatRoom()");
+
+    const chatRoom = document.getElementById("chatRoom");
+    chatRoom.style.height = innerHeight - offsetY + "px";
+}
+
+function createFooter(footerOffsetY) {
+    console.log("createFooter()");
+
+    const footer = document.createElement('div');
+    footer.id = "footer";
+    footer.innerHTML = 'Website: <a href="https://blog.blushyface.com/" target="_blank">blushyface.com</a> | Feedback & suggestions: <a href="https://blushyface.com/discord/" target="_blank"> Discord</a> | Hosted @ ' + document.location.host;
+    footer.style.marginTop = innerHeight - footerOffsetY + "px";
+
+    return footer;
+}
+
+function resizeFooter(offsetY) {
+    console.log("resizeFooter()");
+    footer.style.marginTop = innerHeight - offsetY + "px";
+}
+
+
+
+
+function createChatRoomMessage(from, message) {
+    var divFrom = document.createElement('div');
+    divFrom.id = "chatMessage";
+    divFrom.innerHTML = from + ": " + message;
+
+    var chatRoom = document.getElementById("chatRoomMessages");
+    chatRoom.appendChild(divFrom);
+
+    chatRoom.scrollTop = chatRoom.scrollHeight - chatRoom.clientHeight;
+}
+
+
+function saveRoomDrawing() {
+    console.log("saveRoomDrawing()");
+
+    let download = document.createElement('a');
+    download.setAttribute('download', `${document.getElementById("roomInputName").value}.png`);
+    canvas.toBlob(function (blob) {
+        let url = URL.createObjectURL(blob);
+        download.setAttribute('href', url);
+        download.click();
+    });
+}
+
+
+function getMousePosition(canvas, event) {
+    const rect = canvas.getBoundingClientRect();
+    const x = Math.floor(event.clientX - rect.left);
+    const y = Math.floor(event.clientY - rect.top);
+
+    return [x, y];
+}
+
+
+function hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)];
+    //return result ? {
+    //    r: parseInt(result[1], 16),
+    //    g: parseInt(result[2], 16),
+    //    b: parseInt(result[3], 16)
+    //} : null;
+}
+
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length === 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function setColor(color) {
+    selectedColor = color;
+}
+
+function setStrokeSize(size) {
+    selectedSize = size;
+}
+
+
+
+function undoDrawAction(connId, strokeId) {
+    if (Array.isArray(remoteDrawingHistory) && remoteDrawingHistory.length) {
+        remoteDrawingHistory = remoteDrawingHistory.filter(item => !(item.connId === connId && item.id === strokeId));
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        redraw(remoteDrawingHistory);
+        redraw(localDrawingHistory);
+    }
+}
+
+async function resetRoom() {
+    ResetCanvas(context);
+    remoteDrawingHistory = [];
+    localDrawingHistory = [];
+
+    if (connection.state === signalR.HubConnectionState.Connected) {
+        await connection.invoke("ResetRoom", connId);
+    }
+}
+
+async function sendChatRoomMessage() {
+    if (connection.state === signalR.HubConnectionState.Connected) {
+        var message = document.getElementById('chatRoomInputMessage').value;
+        await connection.invoke("SendMessageToRoom", connId, message);
+    }
+}
+
+
+
+// due to reducing data packet size we are now receiving data as a uInt8 array but we need to convert it.
+function convertUint8ArrayToJson(uInt8Array) {
+    console.log("convertUint8ArrayToJson()");
+
+    var obj = msgpack5({ forceFloat64: true }).decode(uInt8Array);
+    var json = JSON.stringify(obj);
+    return json;
+}
+
+function convertUint8ArrayToObject(uInt8Array) {
+    console.log("convertUint8ArrayToObject()");
+
+    return msgpack5({ forceFloat64: true }).decode(uInt8Array);
+}
